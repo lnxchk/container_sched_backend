d28a5d76ff1bc83129888898a154944001e2b1f9
lnxchk <ubuntu@ip-172-31-6-208.eu-central-1.compute.internal>
2017-08-24 21:44:44 +0000

    Modified the rust application to make use of the cfg.color configuration variable Current version has a lot of output that could be cleaned up if it's too noisy, but this is mainly for demo purposes

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,22 @@ fn main() {
     let mut file = File::open("config/config.toml").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
+	println!("{}", contents);
     let value = contents.parse::<Value>().unwrap();
+	println!("After Parse into Value");
+//	println!("{}", value);
     let five_seconds = time::Duration::new(5, 0);
 
     loop {
-        if value["cfg"]["leader"].as_bool().unwrap() {
+	println!("In Loop");
+        if value["myconfig"]["leader"].as_bool().unwrap() {
             println!("I am a leader");
         } else {
             println!("I am a follower");
         }
+	let color = value["myconfig"]["color"].as_str().unwrap();
+	println!("My favorite color is {}", color);
+
         thread::sleep(five_seconds);
     }
 }
