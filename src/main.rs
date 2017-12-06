aa12feb2fac175f1dd5dc1ec5ef81c528bdfbae7
Mandi Walls <mandi.walls@gmail.com>
2017-12-06 11:04:16 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() {
             println!("I am a follower");
         }
 	let color = value["myconfig"]["color"].as_str().unwrap();
-	println!("My favorite color is {}", color);
+	println!("LinuxLab's favorite color is {}", color);
 
         thread::sleep(five_seconds);
     }
